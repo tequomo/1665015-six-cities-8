d082165cf54c7eca43e47024cb5e13940c87026c
tequomo <tequomo@gmail.com>
2021-10-08 02:18:33 +0300

    ~

---
+++ project/src/mock/reviews.ts
@@ -0,0 +1,50 @@
+export const reviews = [
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': '2019-05-08T14:13:56.569Z',
+    'id': 1,
+    'rating': 4,
+    'user': {
+      'avatar_url': 'img/1.png',
+      'id': 4,
+      'is_pro': false,
+      'name': 'Max',
+    },
+  },
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': '2019-05-08T14:13:56.569Z',
+    'id': 1,
+    'rating': 4,
+    'user': {
+      'avatar_url': 'img/1.png',
+      'id': 4,
+      'is_pro': false,
+      'name': 'Max',
+    },
+  },
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': '2019-05-08T14:13:56.569Z',
+    'id': 1,
+    'rating': 4,
+    'user': {
+      'avatar_url': 'img/1.png',
+      'id': 4,
+      'is_pro': false,
+      'name': 'Max',
+    },
+  },
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': '2019-05-08T14:13:56.569Z',
+    'id': 1,
+    'rating': 4,
+    'user': {
+      'avatar_url': 'img/1.png',
+      'id': 4,
+      'is_pro': false,
+      'name': 'Max',
+    },
+  },
+];
