535a441b5db4db1f37058aa1b941da0e6a17a3f8
tequomo <tequomo@gmail.com>
2021-11-03 01:22:48 +0300

    Реализовал адаптер

--- project/src/services/adapter.ts
+++ project/src/services/adapter.ts
@@ -1,29 +1,28 @@
 import { BackendOfferType, OfferType } from '../types/offer-type';
 
-
 export const adaptToClient = (data: BackendOfferType[]): OfferType[] =>
-  data.map((offer: BackendOfferType) => {
-    const {
-      'is_favorite': del1,
-      'is_premium': del2,
-      'max_adults': del3,
-      'preview_image': del4,
-      ...adaptedProperties
-    } = {
-      ...offer,
+  data.map((offer: BackendOfferType) => (
+    {
+      bedrooms: offer.bedrooms,
+      city: offer.city,
+      description: offer.description,
+      goods: offer.goods,
       host: {
-        ...offer.host,
-        isPro: offer.host['is_pro'],
         avatarUrl: offer.host['avatar_url'],
+        id: offer.host.id,
+        isPro: offer.host['is_pro'],
+        name: offer.host.name,
       },
+      id: offer.id,
+      images: offer.images,
       isFavorite: offer['is_favorite'],
       isPremium: offer['is_premium'],
+      location: offer.location,
       maxAdults: offer['max_adults'],
       previewImage: offer['preview_image'],
-    };
-
-    // delete adaptedOffers.host['avatar_url'];
-    // delete adaptedOffers.host['is_pro'];
-
-    return adaptedProperties;
-  });
+      price: offer.price,
+      rating: offer.rating,
+      title: offer.title,
+      type: offer.type,
+    }
+  ));
